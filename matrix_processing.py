838973ebdc5a97743f261764012923b911554931
Daniil <32582330+danonya666@users.noreply.github.com>
2018-03-25 02:59:06 +0300

    Add files via upload
    Added counting of red pixels in matrix_processing
    Added config file where global variables are stored

--- matrix_processing.py
+++ matrix_processing.py
@@ -1,26 +1,82 @@
-from splitting_frames import *
+from config import *
 from PIL import Image
 
-"""def find_laser(width, height, pix):
-    for i in range(height):
-        counter = 0
-        for j in range(width):
-            rgb = pix[j, i]
-            if (rgb[0] > 80):
-                counter += 1
-            else:
-                if (counter > width // 3)   #if it's more than a third of pixels wide --> YSPEH! Laser found!
-                    laser_matrix =  #let's create a laser matrix here
-                counter = 0
+#def for checking if a pixel is LASER
+def is_laser(pix):
+    if(pix > 80):
+        return 1
+    else:
+        return 0
 
-    return laser_matrix"""
+
+
+def find_laser(width, height, pix):
+    #laser_matrix = [[]] # matrix where the laser pixels lie
+    r_counter = 0 #row counter counts the current row of our laser matrix
+    c_counter = 0 #column counter for the current column of our laser matrix
+    counter = [] # counter of red pixels in a line
+    row_has_laser = [] # true if a row has a red pixel
+
+    # initializing the arrays
+    for i in range(height):
+        counter.append(0)
+        row_has_laser.append(0)
+
+        #countring red dots in lines
+    for i in range(height):
+        for j in range(width):
+            #print(i, j)
+            r = pix[j, i][0] # red component of the pixel
+            g = pix[j, i][1] # green component of the pixel
+            b = pix[j, i][2] # blue component of the pixel
+            row_has_laser = 0  # int for finding out if the row has a laser pixel
+            if (is_laser(r)): # if pixel is red enough
+                #row_has_laser[0] = 1
+                counter[i] += 1
+
+    return counter
+
+"""
+            else:
+                if (counter > width // 3):   #if it's more than a third of pixels wide --> YSPEH! Laser found!
+                    return laser_matrix #Our laser is found, let's return it!
+                else:
+                    laser_matrix = [[]] #if counter is too small and the red part has ended, that means that this is just a glare, not a matrix
+                                        #so we delete our laser_matrix and start looking for the REAL LASER
+                    counter = 0         #Also zeroing some vars
+
+
+"""
+
 
 
 this_image = Image.open('check.png')
-im_columns = this_image.width
-im_rows = this_image.height
+im_columns = this_image.width #Detect width
+im_rows = this_image.height #Detect height
+
+"""
+im_columns = the number of columns in a picture
+im_rows = the number of rows in a picture
+"""
 
 this_pix = this_image.load()    #remember, [columns, rows]
 
+"""
+this_pix[j, i][k] - pixel with 
+width = i 
+height = j
+color attribute = k
+"""
+
+#printing our picture by pixels to output.txt
+#just to check if everything is alright
+for i in range(im_rows):
+    f.write('\n')
+    f.write('\n')
+    f.write(str(i) +  " - " + str(find_laser(im_columns, im_rows, this_pix)[i]))
+    for j in range(im_columns):
+        #f.write('[' + str(this_pix[j, i][0]) + " " + str(this_pix[j, i][1]) + " " + str(this_pix[j, i][2]) + ']') #Every pixel is [R, G, B]
+        #f.write(str(i) + "-" + str(j) + " ") #For checking the indexation
+        pass
 
 
