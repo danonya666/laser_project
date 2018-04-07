b016eb41cb3b7e8858c8e22874543f65c1704236
Daniil <32582330+danonya666@users.noreply.github.com>
2018-04-07 23:19:57 +0300

    moved functions to config
    made a cool printing to file function, changed some names of functions

--- config.py
+++ config.py
@@ -1,7 +1,88 @@
 import sys
+import datetime
+from PIL import Image
 sys.path.insert(0, "Lib\site-packages")
 
 f = open('another.txt', 'w')
-f.write('\n')
 
-#This will split movie on frames -> to process them in matrix_processing -> to analyse red pixels matrix
+def is_laser(red_component):
+    if(red_component > 70):
+        return 1
+    else:
+        return 0
+
+# should be used like find_laser(im_columns, im_rows, this_pix)
+# returns a list with the number of red pixels in every row
+def red_component_in_lines(width, height, pix):
+    counter = [] # counter of red pixels in a line
+        #countring red dots in lines
+    for i in range(height):
+        counter.append(0) # initializing the lists
+        for j in range(width):
+            r = pix[j, i][0] # red component of the pixel
+            if (is_laser(r)): # if pixel is red enough
+                counter[i] += 1
+
+    return counter
+
+
+# function for returning the place of the laser
+def find_laser_matrix_place(counter, height):
+    num = 0 # Here we gonna store the position of laser
+    square = [] # list of squares of suspicions to laser
+    i = 0
+    while(i < height):
+        if(counter[i]):  # if a line has red dots
+            square.append([0, num]) # initializing a place in square list for this object
+            while(i < height and counter[i] > 0):
+                square[num][0] += counter[i] # counting the square of the object
+                i += 1
+            num += 1 # going to the next item in square list
+        i += 1
+    square.sort(reverse = True) # sorting squares, biggest square is in [0]
+    return square[0][1] # returning the place of the laser
+
+# function for returning the medium intensity of the laser
+# SHOULD BE RENAMED
+# position - result of the function find_laser_matrix_place
+# counter - result of the function find_laser
+def medium_intensity(pix, position, width, height, counter):
+    num = 0
+    i = 0 # putting i counter to the place of the laser
+    while(num != position or counter[i] == 0):
+        if(counter[i] > 0):
+            i += 1
+            while(counter[i] != 0):
+                i += 1
+            num += 1
+        i += 1
+
+    red_sum = 0 # sum of the red component in a laser
+    pixel_counter = 0 # quantity of pixels in a laser
+    intensity = 0 # = red_sum / pixel_counter
+    j = 0
+    while(counter[i] != 0 and i < height - 1): # while we are in the laser
+        while(j < width): # while we are in the row
+            while(not is_laser(pix[j, i][0]) and j < width - 1):
+                j += 1 # this skips the black dots in a row
+            red_sum += pix[j, i][0]
+            pixel_counter += 1
+            j += 1
+        i += 1
+        j = 0
+    intensity = red_sum / pixel_counter
+    return intensity
+
+#print(get_laser_matrix(this_pix, position, im_columns, im_rows, counter)) # printing the intensity
+def multiple_images_to_file(quantity, basic_location, name, ending):
+    f.write("Logging the changes in laser intensity\n//////////////////")
+    for i in range(quantity):
+        this_image = Image.open(basic_location + "\\" + name + str(i) + ending)
+        this_pix = this_image.load()
+        im_columns = this_image.width  # Detect width
+        im_rows = this_image.height  # Detect height
+        counter = red_component_in_lines(im_columns, im_rows, this_pix)  # the list with the numbers of red dots in each row
+        position = find_laser_matrix_place(counter, im_rows)  # the place of the matrix
+        current_time = str(datetime.datetime.now())
+        f.write(str("%.2f" % medium_intensity(this_pix, position, im_columns, im_rows, counter)) + ' - ' + current_time + '\n')   # printing the intensity
+    f.write("//////////////////" + '\n' + "end of log")
--- matrix_processing.py
+++ matrix_processing.py
@@ -1,114 +1,13 @@
 from config import *
-from PIL import Image
 
-#def for checking if a pixel is LASER
-def is_laser(pix):
-    if(pix > 70):
-        return 1
-    else:
-        return 0
-
-# should be used like find_laser(im_columns, im_rows, this_pix)
-# returns a list with the number of red pixels in every row
-def find_laser(width, height, pix):
-    counter = [] # counter of red pixels in a line
-
-    # initializing the lists
-    for i in range(height):
-        counter.append(0)
-
-        #countring red dots in lines
-    for i in range(height):
-        for j in range(width):
-            r = pix[j, i][0] # red component of the pixel
-            g = pix[j, i][1] # green component of the pixel
-            b = pix[j, i][2] # blue component of the pixel
-            if (is_laser(r)): # if pixel is red enough
-                counter[i] += 1
-
-    return counter
-
-# function for returning the place of the laser
-def find_laser_matrix_place(counter, height):
-    num = 0 # Here we gonna store the position of laser
-    square = [] # list of squares of suspicions to laser
-    i = 0
-    while(i < height):
-        if(counter[i] > 0):  # if a line has red dots
-            square.append([0, num]) # initializing a place in square list for this object
-            while(i < height and counter[i] > 0):
-                square[num][0] += counter[i] # counting the square of the object
-                i += 1
-            num += 1 # going to the next item in square list
-        i += 1
-    square.sort(reverse = True) # sorting squares, biggest square is in [0]
-    return square[0][1] # returning the place of the laser
-
-# function for returning the medium intensity of the laser
-# SHOULD BE RENAMED
-# position - result of the function find_laser_matrix_place
-# counter - result of the function find_laser
-def get_laser_matrix(pix, position, width, height, counter):
-    num = 0
-    i = 0 # putting i counter to the place of the laser
-    while(num != position or counter[i] == 0):
-        if(counter[i] > 0):
-            i += 1
-            while(counter[i] != 0):
-                i += 1
-            num += 1
-        i += 1
-
-    red_sum = 0 # sum of the red component in a laser
-    pixel_counter = 0 # quantity of pixels in a laser
-    intensity = 0 # = red_sum / pixel_counter
-    j = 0
-    while(counter[i] != 0 and i < height - 1): # while we are in the laser
-        while(j < width): # while we are in the row
-            while(not is_laser(pix[j, i][0]) and j < width - 1):
-                j += 1 # this skips the black dots in a row
-            red_sum += pix[j, i][0]
-            pixel_counter += 1
-            j += 1
-        i += 1
-        j = 0
-    intensity = red_sum / pixel_counter
-    return intensity
+multiple_images_to_file(50, "screenshots", "dtest", ".png")
 
 
-
-
+#this_pix = this_image.load()    #remember, [columns, rows]
 """
 im_columns = the number of columns in a picture
 im_rows = the number of rows in a picture
 """
-
-#this_pix = this_image.load()    #remember, [columns, rows]
-
-#print(get_laser_matrix(this_pix, position, im_columns, im_rows, counter)) # printing the intensity
-
-sum41 = 0
-values = []
-kek = 50
-for i in range(kek):
-    this_image = Image.open('screenshots\\dtest' + str(i) + '.png')
-    this_pix = this_image.load()
-    im_columns = this_image.width  # Detect width
-    im_rows = this_image.height  # Detect height
-    counter = find_laser(im_columns, im_rows, this_pix)  # the list with the numbers of red dots in each row
-    position = find_laser_matrix_place(counter, im_rows)  # the place of the matrix
-    f.write(str(get_laser_matrix(this_pix, position, im_columns, im_rows, counter)) + '\n')   # printing the intensity
-    values.append(get_laser_matrix(this_pix, position, im_columns, im_rows, counter))
-    sum41 += (get_laser_matrix(this_pix, position, im_columns, im_rows, counter))
-
-sum41 = sum41 / kek
-delta = 0
-for i in values:
-    delta += abs(i - sum41)
-f.write("delta = " + str(delta / kek) + '\n')
-f.write("end of test" + "\n" + "//////////////////" + '\n')
-
-#print(delta)
 """
 #Just some printing into a file
 for i in range(im_rows):
